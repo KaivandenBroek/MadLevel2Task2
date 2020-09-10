2c8d1b527f337b90ef611f6f2a616c3dc6463530
Kai <Kaivandenbroek@hotmail.com>
2020-09-10 22:17:18 +0200

    code progress

--- app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.madlevel2task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val questions = arrayListOf<Question>()
+    private val questionsAdapter = QuestionsAdapter(questions)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +23,52 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        binding.rvQuestions.layoutManager = StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL)
+        binding.rvQuestions.adapter = questionsAdapter
 
+        for (i in Question.QUESTIONS.indices) {
+            questions.add(Question(Question.QUESTIONS[i],
+                Question.QUESTION_AWNSER[i]))
+        }
+        questionsAdapter.notifyDataSetChanged()
+    }
+
+    private fun createItemTouchHelper() : ItemTouchHelper {
+        val callbackLeft = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                questions.removeAt(position)
+                questionsAdapter.notifyDataSetChanged()
+            }
+
+        }
+
+        val callbackRight = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                questions.removeAt(position)
+                questionsAdapter.notifyDataSetChanged()
+            }
+
+        }
+
+        // returns the outcome of the question
+        return ItemTouchHelper(callbackLeft)
     }
 }
